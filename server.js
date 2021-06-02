9b45734425577cffd36f81b36c286e71652c3fab
tsn_nagarajan <36725099+Nagarajan-ThammannaSrinivasan@users.noreply.github.com>
2021-06-02 15:43:35 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const express = require('express');
+const { Server } = require('ws');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  // .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  // .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+// setInterval(() => {
+//   wss.clients.forEach((client) => {
+//     client.send(new Date().toTimeString());
+//   });
+// }, 1000);
+
+server.use(express.urlencoded({
+  extended: true
+}));
+
+
+server.use(express.json())
+
+
+ server.post('/postLocation',  (req, res) =>{
+    console.log(req.body)
+    res.end(JSON.stringify(req.body))
+    
+    wss.clients.forEach(client =>{        
+        client.send(JSON.stringify(req.body))
+    })
+})
